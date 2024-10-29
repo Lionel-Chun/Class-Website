a5d404b5dcc7b37f3fab7da67991be9088faf03b
Lionel Chun <lionel.chun@outlook.com>
2024-10-29 16:21:13 +0800

    2024-10-29_beta-02

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,15 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
-});
-
-router.get('/add', function (req, res, next) {
+}).get('/add', function (req, res, next) {
   res.send('this is to handle /users/add');
-});
-
-router.get('/json', function(req, res, next) {
+}).get('/json', function(req, res, next) {
   res.json({monday:"bread", tuesday:"sandwich"});
+}).get('/testPara', function(req, res, next) {
+  for (let q in req.query)
+    console.log(q + " is " + req.query[q]);
+    //console.log("a:", req.query.a, "b:", req.query.b);
+    res.send("Done!");
 });
 
 module.exports = router;
