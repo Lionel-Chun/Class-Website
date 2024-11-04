5f531d42d094f97e62af41e47579b984278bcd33
Lionel Chun <lionel.chun@outlook.com>
2024-11-04 17:39:25 +0800

    2024-11-04_beta-05

--- routes/teacher.js
+++ routes/teacher.js
@@ -15,11 +15,11 @@ const client = new MongoClient(uri,  {
     }
 );
 
-const teacher = [
-    {name:'Alex',tel:'91234567',sex:'M',age:30,course:['eng', 'chi']},
-    {name:'Betty',tel:'92345678',sex:'F',age:28,course:['chem']},
-    {name:'Carie',tel:'61234567',sex:'F',age:50,course:['history']}
-];
+// const teacher = [
+//     {name:'Alex',tel:'91234567',sex:'M',age:30,course:['eng', 'chi']},
+//     {name:'Betty',tel:'92345678',sex:'F',age:28,course:['chem']},
+//     {name:'Carie',tel:'61234567',sex:'F',age:50,course:['history']}
+// ];
 
 router.get('/createDB', async function(req, res, next) {
 
@@ -28,7 +28,7 @@ router.get('/createDB', async function(req, res, next) {
     await client.connect();
     // let db = client.db("mdb");
     // db.dropCollection("teacher");
-    // await client.db("mdb").collection("teacher").deleteMany({});
+    await client.db("mdb").collection("teacher").deleteMany({});
     await client.db("mdb").collection("teacher").insertOne(
       {name:'Alex',tel:'91234567',sex:'M',age:30,course:['eng', 'chi']}
     );
@@ -72,7 +72,6 @@ router.get('/createDB', async function(req, res, next) {
 
       // for (let f in teacher) if (f!="_id") s+= f + ": " + teacher[f] + "<br>"; 
       for (let f in teacher) s+= f + ": " + teacher[f] + "<br>";
-
       res.send("<h3>Detail of teacher " + req.query.name + "</h3>" + s);   
     } finally {
       await client.close();
@@ -89,24 +88,43 @@ router.get('/createDB', async function(req, res, next) {
     // }
     // res.send("Cannot find matching teacher");
 
-}).post('/detail', function(req, res, next) {
-  let name = req.body.name;
-  let tel = req.body.tel;
-  let sex = req.body.sex;
-  let age = req.body.age;
-  let course = Object.values(req.body.course);
-  let newTeacher = {name:name,tel:tel,sex:sex,age:age,course:course};
-  teacher[teacher.length] = newTeacher;
-  //console.log(teacher);
-  let html = '<h1>Teacher List</h1>';
-  for(let num in teacher) {
-    html += 'Name: ' + teacher[num].name + '<br>';
-    html += 'Tel: ' + teacher[num].tel + '<br>' ;
-    html += 'Sex: ' + teacher[num].sex + '<br>' ;
-    html += 'Age: ' + teacher[num].age + '<br>' ;
-    html += 'Course: ' + teacher[num].course.toString() + '<br>' ;
-    html += '<hr>';
-  }
-  res.send(html);
 });
+
+router.post('/insert', async function(req, res, next) {
+
+  console.log(req.body);
+  
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    await client.db("mdb").collection("teacher").insertOne(
+      req.body
+    );
+    res.send("record inserted");
+  } finally {
+    await client.close();
+  }
+});
+
+
+// router.post('/detail', function(req, res, next) {
+//   let name = req.body.name;
+//   let tel = req.body.tel;
+//   let sex = req.body.sex;
+//   let age = req.body.age;
+//   let course = Object.values(req.body.course);
+//   let newTeacher = {name:name,tel:tel,sex:sex,age:age,course:course};
+//   teacher[teacher.length] = newTeacher;
+//   //console.log(teacher);
+//   let html = '<h1>Teacher List</h1>';
+//   for(let num in teacher) {
+//     html += 'Name: ' + teacher[num].name + '<br>';
+//     html += 'Tel: ' + teacher[num].tel + '<br>' ;
+//     html += 'Sex: ' + teacher[num].sex + '<br>' ;
+//     html += 'Age: ' + teacher[num].age + '<br>' ;
+//     html += 'Course: ' + teacher[num].course.toString() + '<br>' ;
+//     html += '<hr>';
+//   }
+//   res.send(html);
+// });
 module.exports = router;
