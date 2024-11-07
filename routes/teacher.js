6fa06e497e7c618fbc8733817b2bd91663c087e3
Lionel Chun <lionel.chun@outlook.com>
2024-11-07 11:17:55 +0800

    2024-11-07_Beta-03

--- routes/teacher.js
+++ routes/teacher.js
@@ -36,6 +36,23 @@ router.get('/', async function(req, res, next) { // list teachers name
 
 });
 
+router.post('/add', async function(req, res, next) {
+
+  console.log(req.body);
+  req.body.age = Number.parseInt(req.body.age);
+
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    await client.db("mdb").collection("teacher").insertOne(
+      req.body
+    );
+    res.send("record inserted");
+  } finally {
+    await client.close();
+  }
+});
+
 router.get('/createDB', async function(req, res, next) {
 
   try {
@@ -76,6 +93,7 @@ router.get('/detail', async function(req, res, next) {
 
     // for (let f in teacher) if (f!="_id") s+= f + ": " + teacher[f] + "<br>"; 
     for (let f in teacher) s+= f + ": " + teacher[f] + "<br>";
+
     res.send("<h3>Detail of teacher " + req.query.name + "</h3>" + s);   
   } finally {
     await client.close();
@@ -87,8 +105,6 @@ router.get('/edit', async function (req, res, next) {
   try {
     await client.connect();
     let teacher =  await client.db("mdb").collection("teacher").findOne(query);
-    // console.log(teacher);
-    // console.log(teacher.course.chem != undefined);
     res.render("teacher", teacher);
   } finally {
     await client.close();
@@ -96,102 +112,23 @@ router.get('/edit', async function (req, res, next) {
 });
 
 router.post('/edit', async function (req, res, next) {
+
   // @deprecated Instead, use `static createFromTime()` to set a numeric value for the new ObjectId.
   // const query = {_id: new ObjectId(req.body._id)};
-
   const query = {_id: ObjectId.createFromHexString(req.body._id)};
-  console.log(query);
+
   try {
     await client.connect();
     let teacher = await client.db("mdb").collection("teacher").findOne(query), s = "";
-    //req.body.course = req.body.course.splite(",");
-    // console.log(req.body.course);
-    console.log("*************teacher**************");
-    console.log(teacher);
     let newTeacher = {};
     for (let f in teacher) {
       if (f != "_id") newTeacher[f] = req.body[f];
     }
 
-    console.log("*************newTeacher**************");
-    console.log(query);
-    console.log(newTeacher);
-
     await client.db("mdb").collection("teacher").updateOne(
       query, {$set:newTeacher}
     );
     res.redirect("/teacher");
-  } finally {
-    await client.close();
-  }
-});
-
-// router.post('/edit', async function (req, res, next) {
-
-//   try {
-//     await client.connect();
-//     let teacher = await client.db("mdb").collection("teacher").findOne({
-//       name: req.body.origName
-//     }), s = "";
-//     req.body.course = req.body.course.splite(",");
-//     req.body.age = Number.parseInt(req,body.age);
-//     let newTeacher = {};
-//     for (let f in teacher) if (f != "_id") newTeacher[f] = req.body[f];
-//     await client.db("mdb").collection("teacher").updateOne(
-//       {name: req.body._id }, {$set:newTeacher}
-//     );
-//     res.redirect("/teacher");
-//   } finally {
-//     await client.close();
-//   }
-  
-//   try {
-//     console.log(req.body);
-//     console.log(req.body._id);
-//     console.log(req.body.name);
-//     console.log(req.body.tel);
-//     console.log(req.body.sex);
-//     console.log(req.body.course);
-
-//   } finally {
-//     res.end('ok');
-//     await client.close();
-//   }
-// });
-
-// router.post('/detail', function(req, res, next) {
-//   let name = req.body.name;
-//   let tel = req.body.tel;
-//   let sex = req.body.sex;
-//   let age = req.body.age;
-//   let course = Object.values(req.body.course);
-//   let newTeacher = {name:name,tel:tel,sex:sex,age:age,course:course};
-//   teacher[teacher.length] = newTeacher;
-//   //console.log(teacher);
-//   let html = '<h1>Teacher List</h1>';
-//   for(let num in teacher) {
-//     html += 'Name: ' + teacher[num].name + '<br>';
-//     html += 'Tel: ' + teacher[num].tel + '<br>' ;
-//     html += 'Sex: ' + teacher[num].sex + '<br>' ;
-//     html += 'Age: ' + teacher[num].age + '<br>' ;
-//     html += 'Course: ' + teacher[num].course.toString() + '<br>' ;
-//     html += '<hr>';
-//   }
-//   res.send(html);
-// });
-
-router.post('/add', async function(req, res, next) {
-
-  console.log(req.body);
-  req.body.age = Number.parseInt(req.body.age);
-
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    await client.db("mdb").collection("teacher").insertOne(
-      req.body
-    );
-    res.send("record inserted");
   } finally {
     await client.close();
   }
